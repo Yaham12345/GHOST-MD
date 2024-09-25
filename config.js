6644b2c9e2a7bdbe25ad933366f75bfea58fe92d
Yaham12345 <m44266904@gmail.com>
2024-09-25 13:19:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put season id",
+SESSION_ID: process.env.SESSION_ID || "LJkGiITa#o3Lre_Fhp_2ozMK_ydx8pdKsWx7iv4t0goSh4BKkTfs",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
-SUDO_NB: process.env.SUDO_NB || "94741140620",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi Yaham-MD Is Online Now 💻\n*💻 Owner* - Yaham-MD\n\n*💻 Owner Number* -94756209818",
+SUDO_NB: process.env.SUDO_NB || "94756209818",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
